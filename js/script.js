cde8a3d5b14c3bf81175d1b0c6a8fb773c248929
JBatsyus <ju.kalabina@gmail.com>
2026-04-08 10:55:22 +0200

    страница мебель

---
+++ js/script.js
@@ -0,0 +1,29 @@
+// Инициализация слайдеров для всех карточек
+document.addEventListener('DOMContentLoaded', () => {
+    const furnishingSlider = new Swiper('.b2b-furnishing__slider', {
+        slidesPerView: 1.001, // Показывает часть следующего слайда
+        spaceBetween: 8,
+        loop: true,
+        grabCursor: true,
+        speed: 1500,
+        // autoplay: {
+        //     delay: 5000,
+        //     disableOnInteraction: false,
+        //     pauseOnMouseEnter: true,
+        // },
+        navigation: {
+            nextEl: '.b2b-furnishing__arrow--next',
+            prevEl: '.b2b-furnishing__arrow--prev',
+        },
+        breakpoints: {
+            768: {
+
+                spaceBetween: 20,
+            },
+            1440: {
+                slidesPerView: 2,
+                spaceBetween: 24,
+            }
+        }
+    });
+});
