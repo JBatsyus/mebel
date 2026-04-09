bc9368c5002fd4a741bfe7a7b2b17455e007a44c
JBatsyus <ju.kalabina@gmail.com>
2026-04-09 09:40:38 +0200

    код-ревью 1

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // Инициализация слайдеров для всех карточек
 document.addEventListener('DOMContentLoaded', () => {
     const furnishingSlider = new Swiper('.b2b-furnishing__slider', {
-        slidesPerView: 1.001, // Показывает часть следующего слайда
+        slidesPerView: 1.001, 
         spaceBetween: 8,
         loop: true,
         grabCursor: true,
@@ -77,7 +77,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
         item.innerHTML = `
         <span>${file.name}</span>
-        <button type="button" title="Удалить">
+        <button type="button" title="Удалить" aria-label="Удалить">
             <svg width="14" height="14" viewBox="0 0 19 19" fill="none" style="transform: rotate(45deg);">
                 <path d="M9.38 0C9.92 0 10.36 0.43 10.36 0.98V8.4H17.78C18.32 8.4 18.76 8.83 18.76 9.38C18.76 9.92 18.32 10.36 17.78 10.36H10.36V17.78C10.36 18.32 9.92 18.76 9.38 18.76C8.83 18.76 8.4 18.32 8.4 17.78V10.36H0.98C0.43 10.36 0 9.92 0 9.38C0 8.83 0.43 8.4 0.98 8.4H8.4V0.98C8.4 0.43 8.83 0 9.38 0Z" fill="#A9A9A9" />
             </svg>
